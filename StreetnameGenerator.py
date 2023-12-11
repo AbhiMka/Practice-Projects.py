52066cad8fa58747f274304f35408ddc6681e4fd
AbhiMka <153517407+AbhiMka@users.noreply.github.com>
2023-12-11 18:31:35 +0530

    Update StreetnameGenerator.py

--- StreetnameGenerator.py
+++ StreetnameGenerator.py
@@ -1,4 +1,4 @@
 print("Welcome to the Streetname Name Generator.")
-shirt = input("What's the color of the shirt you're wearing rightnow?\n")
+shirt = input("What's the color of the shirt you're wearing right now?\n")
 food = input("What's the name of the food item you last ate?\n")
 print("Your street name could be " + shirt + " " + food)
